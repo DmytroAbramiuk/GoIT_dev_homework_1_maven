d46b20a954e837acf363d28ca45e2dd36bdfa6ab
Oleh Abramiuk <dima.abramiuk@gmail.com>
2023-10-11 21:12:28 +0300

    initial commit

--- src/main/java/org/example/Human.java
+++ src/main/java/org/example/Human.java
@@ -8,20 +8,4 @@ public class Human {
         this.name = name;
         this.lastName = lastName;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
